d2402ecf635673f3c06be1fcff3eb8b895c8931f
Teddy Weisman <tjweisman@gmail.com>
2019-01-31 19:19:47 -0600

    added dependencies and metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
+from os import path
+
+setup_dir = path.abspath(path.dirname(__file__))
+with open(path.join(setup_dir, 'README.md'),
+          encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name="cr_download",
-    version="1.0",
+    version="0.9",
     packages=find_packages(),
     scripts=["bin/critrole_download",
              "bin/autocut_vod"],
@@ -11,5 +18,24 @@ setup(
             'share/sound_files/*']
     },
 
-    author="Teddy Weisman"
+    install_requires=[
+        'pyacoustid',
+        'streamlink',
+        'tqdm',
+        'pyyaml',
+        'requests',
+        'google-api-python-client',
+        'progressbar2'
+    ],
+
+    author="Teddy Weisman",
+    author_email="tjweisman@gmail.com",
+    license='MIT',
+    url="https://github.com/tjweisman/cr_download",
+
+    description="""Tool to download Critical Role episodes from
+    the Geek & Sundry Twitch channel, convert to audio, and recut.""",
+
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
